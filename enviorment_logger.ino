239c892029d70f761b2fa3b990e44f5f7847492b
CraCaN <50067585+CraCaNN@users.noreply.github.com>
2024-09-12 13:43:09 +0100

    Some improvments
    AEL can now opperate with or without the SD card, the OLED and without Wi-Fi

--- enviorment_logger.ino
+++ enviorment_logger.ino
@@ -1,9 +1,20 @@
+//Configurable Settings
+
+//Set to true to show more information on OLED 
+bool moreInfo = false;
+
+//Enter name of the file to be written to the SD card
+String logName = "AEL.csv";
+
+
+
 //Getting time through NTP
 #include <NTPClient.h>
 //Wifi Module
 #include <WiFiNINA.h>
 //UDP for NTP Time
 #include <WiFiUdp.h>
+
 //MKR ENV Shield
 #include <Arduino_MKRENV.h>
 //SPI
@@ -18,7 +29,8 @@
 #include <Adafruit_GFX.h>
 //SSD OLED 1306 Libary
 #include <Adafruit_SSD1306.h>
-//OLED Setup
+
+//OLED paramaters
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
 
@@ -61,25 +73,23 @@ void ledSetup() {
   //Onboard LEDs setup
   WiFiDrv::pinMode(25, OUTPUT); //Green
   WiFiDrv::digitalWrite(25, HIGH); //GREEN
-  delay(200);
+  delay(100);
   WiFiDrv::digitalWrite(25, LOW); //GREEN
   
   WiFiDrv::pinMode(26, OUTPUT); //Red
   WiFiDrv::digitalWrite(26, HIGH);   //RED
-  delay(200);
+  delay(100);
   WiFiDrv::digitalWrite(26, LOW);   //RED
 
   WiFiDrv::pinMode(27, OUTPUT); //Blue
   WiFiDrv::digitalWrite(27, HIGH);   //BLUE
-  delay(200);
+  delay(100);
   WiFiDrv::digitalWrite(27, LOW);   //BLUE
-
-  delay(200);
 
   WiFiDrv::digitalWrite(25, HIGH); //GREEN
   WiFiDrv::digitalWrite(26, HIGH);   //RED
   WiFiDrv::digitalWrite(27, HIGH);   //BLUE
-  delay(200);
+  delay(100);
   WiFiDrv::digitalWrite(25, LOW); //GREEN
   WiFiDrv::digitalWrite(26, LOW);   //RED
   WiFiDrv::digitalWrite(27, LOW);   //BLUE
@@ -90,15 +100,18 @@ void ledSetup() {
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void oledSetup() {
-    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   // Clear the buffer
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;  // Don't proceed, loop forever
+  }
+  Serial.println("clear display");
+  
   display.clearDisplay();
-
+  
+  Serial.println("draw pixel");
   // Draw a single pixel in white
   display.drawPixel(10, 10, SSD1306_WHITE);
 
@@ -111,58 +124,90 @@ void oledSetup() {
   display.print("AEL Booting|       |");
   display.display(); 
   delay(200);
-
 }
 
-void netSetup() {
-  //Try to connect to NB 2.4G
-  while (status != WL_CONNECTED) {
+void cDis(){//Clear display and buffer
+  display.clearDisplay();
+  display.setCursor(0, 0);
+}
 
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.println("AEL Booting|-      |");
-    display.println("Connecting to:");
+bool hasNet = false;
+void netSetup() {
+  //Try to connect to Network
+  while (status != WL_CONNECTED) {
+    cDis();
+    display.println("AEL Booting|-      |\nConnecting to:");
     display.println(ssid);
+    display.print("Attempt ");
+    display.print(count+1);
+    display.print("/5");
     display.display();
 
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
+    
+    status = WiFi.begin(ssid, pass);// Connect to WPA/WPA2 network:
     WiFiDrv::digitalWrite(25, HIGH); //GREEN
     WiFiDrv::digitalWrite(26, HIGH);   //RED
     delay(100);
     WiFiDrv::digitalWrite(25, LOW); //GREEN
     WiFiDrv::digitalWrite(26, LOW);   //RED
-    delay(5000);
+    delay(1000);
+    
+    Serial.println(status);
+
+    if (count == 4 && status == 4) {
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.println("Unable to connect to:");
+      display.println(ssid);
+      display.println("Timestamps inaccurate");
+      display.println("Web page unavailable");
+      display.display();
+      delay(4000);
+      break;
+    }
+    else if (status == 4) {
+      pass;
+    }
+    else {
+      IPAddress ip = WiFi.localIP();
+      Serial.print("IP Address: ");
+      Serial.println(ip);
+      Serial.println("You're connected to the network");
+      
+      //display.clearDisplay();
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.println("AEL Booting|--     |");
+      display.println("Connected to WiFi");
+      display.print("IP: ");
+      display.println(ip);
+      display.display();
+
+      hasNet = true;
+      //Show lights to indicate successful connection
+      WiFiDrv::digitalWrite(25, HIGH); //GREEN
+      WiFiDrv::digitalWrite(27, HIGH); //BLUE
+      delay(500);
+      WiFiDrv::digitalWrite(25, LOW); //GREEN
+      WiFiDrv::digitalWrite(27, LOW); //BLUE
+      delay(4000);
+    }
+    count = count + 1;
   }
+  count = 0;
 
-  //Show lights to indicate successful connection
-
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-  Serial.println("You're connected to the network");
   
-  //display.clearDisplay();
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println("AEL Booting|--     |");
-  display.println("Connected to WiFi");
-  display.print("IP: ");
-  display.println(ip);
-  display.display();
+  
+}
 
-  WiFiDrv::digitalWrite(25, HIGH); //GREEN
-  WiFiDrv::digitalWrite(27, HIGH); //BLUE
-  delay(500);
-  WiFiDrv::digitalWrite(25, LOW); //GREEN
-  WiFiDrv::digitalWrite(27, LOW); //BLUE
-  delay(4000);
+void inetTest() {//check that there is still internet before updating NTP time
+  //todo 
 }
 
 void envSetup() {
-  //init the ENV Shield//
+  //init the MKR ENV Shield//
   display.clearDisplay();
   display.setCursor(0, 0);
   display.println("AEL Booting|---    |");
@@ -179,6 +224,8 @@ void envSetup() {
   }
 }
 
+bool cardStatus = false;
+
 void sdSetup() {
   display.clearDisplay();
   display.setCursor(0, 0);
@@ -189,28 +236,34 @@ void sdSetup() {
     //show purple if sd is not detected
     WiFiDrv::digitalWrite(26, HIGH);   //RED
     WiFiDrv::digitalWrite(27, HIGH);   //BLUE
-    display.println("Failed to initialize\nSD Card!");
+    display.println("Unable to detect card\nLogging disabled");
     display.display();
-    while (1);
-  display.println("SD Card Found");
-  display.display();
-  delay(1000);
+    delay(3000);
+    WiFiDrv::digitalWrite(26, LOW);   //RED
+    WiFiDrv::digitalWrite(27, LOW);   //BLUE
+  }
+  else {
+    display.println("SD Card Found");
+    display.display();
+    cardStatus = true; //logging is now enabled
   }
 }
+
 
 void webServer() {
   // listen for incoming clients
   WiFiClient client = server.available();
   if (client) {
-    Serial.println("New client");
-    // an HTTP request ends with a blank line
+    Serial.println("Web page requested");
     WiFiDrv::digitalWrite(26, HIGH);   //RED
     WiFiDrv::digitalWrite(27, HIGH);   //BLUE
+
+
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        Serial.write(c);
+        //Serial.write(c);//Print the request
         // if you've gotten to the end of the line (received a newline
         // character) and the line is blank, the HTTP request has ended,
         // so you can send a reply
@@ -259,7 +312,7 @@ void webServer() {
 
     // close the connection:
     client.stop();
-    Serial.println("client disconnected");
+    Serial.println("Page sent");
     WiFiDrv::digitalWrite(26, LOW);   //RED
     WiFiDrv::digitalWrite(27, LOW);   //BLUE
   }
@@ -267,65 +320,89 @@ void webServer() {
 
 
 void setup() {
+  delay(2000);//delay to allow time for serial to connect
   //LED setup
   ledSetup();
+  Serial.println("LED boot complete");
 
   //OLED setup
   oledSetup();
+  Serial.println("Oled initiallised");
 
   //WiFi Network Setup
   netSetup();
+  Serial.println("Network started");
+  
 
   //Init the ENV Sheild
   envSetup();
+  Serial.println("Shield started");
   
-
-  //Start NTP client
-  timeClient.begin();
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println("AEL Booting|----   |\nStarting NTP Client");
-  display.display();
-  delay(500);
+  
+  if (hasNet == true){
+    //Start NTP client
+    timeClient.begin();
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("AEL Booting|----   |\nStarting NTP Client");
+    display.display();
+    delay(200);
+  }
+  else {
+    timeClient.begin();
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("AEL Booting|----   |\nNo Internet\nTime will be\n00:00:00");
+    display.display();
+    delay(1000);
+  }
 
   // init SPI
   SPI.begin();
   display.clearDisplay();
   display.setCursor(0, 0);
-  display.println("AEL Booting|-----  |\nStarting SPI");
+  display.println("AEL Booting|-----  |\nStarted SPI");
   display.display();
-  delay(500);
+  delay(200);
 
   //SD card setup
   sdSetup();
 
+  if (cardStatus == true) {
   // init the logfile
-  dataFile = SD.open("testing.csv", FILE_WRITE);
-  delay(1000);
-  // init the CSV file with headers
-  dataFile.println("Time,Temperature,Humidity,Light,UVA,UVB,UVI");
-  // close the file
-  dataFile.close();
-  //White LED
+    dataFile = SD.open(logName, FILE_WRITE);
+    delay(1000);
+    // init the CSV file with headers
+    dataFile.println("Time,Temperature,Humidity,Light,UVA,UVB,UVI");
+    // close the file
+    dataFile.close();
+    //White LED
+  }
   
   //Start Web server
-  server.begin();
-
+  if (hasNet == true){
+    server.begin();
+    WiFiDrv::digitalWrite(26, HIGH);   //RED
+    WiFiDrv::digitalWrite(27, HIGH);   //BLUE
+    WiFiDrv::digitalWrite(25, HIGH); //GREEN
+    delay(1000);
+    WiFiDrv::digitalWrite(25, LOW); //GREEN
+    WiFiDrv::digitalWrite(26, LOW);   //RED
+    WiFiDrv::digitalWrite(27, LOW);   //BLUE
+  }
+  else {
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("AEL Booting |-------|");
+    display.println("Skipping webserver start");
+    display.display();
+    delay(1000);
+  }
   display.clearDisplay();
   display.setCursor(0, 0);
   display.println("AEL Booted |-------|");
   display.display();
-
-  WiFiDrv::digitalWrite(26, HIGH);   //RED
-  WiFiDrv::digitalWrite(27, HIGH);   //BLUE
-  WiFiDrv::digitalWrite(25, HIGH); //GREEN
-  delay(1000);
-  WiFiDrv::digitalWrite(25, LOW); //GREEN
-  WiFiDrv::digitalWrite(26, LOW);   //RED
-  WiFiDrv::digitalWrite(27, LOW);   //BLUE
-  delay(1000);
-
-
+  delay(1500);
 }
 //END OF SETUP//
 
@@ -333,7 +410,7 @@ void setup() {
 
 void loop() {
   WiFiDrv::digitalWrite(26, HIGH); //RED
-  timeClient.update();
+  timeClient.update();//update time
   WiFiDrv::digitalWrite(26, LOW); //RED
 
   temperature = ENV.readTemperature();
@@ -346,7 +423,6 @@ void loop() {
   uvI = ENV.readUVIndex();
 
   while (count2 != 10) {
-    WiFiDrv::digitalWrite(27, HIGH); //BLUE
     count = 0;
     
     // read the sensors values
@@ -362,28 +438,28 @@ void loop() {
     //format time
     ntpftime = timeClient.getFormattedTime();
     
-
-    // open the logfile
-    dataFile = SD.open("Benji.csv", FILE_WRITE);
-
-    // write to sd card each of the sensor values
-    dataFile.print(ntpftime);
-    dataFile.print(",");
-    dataFile.print(temperature);
-    dataFile.print(",");
-    dataFile.print(humidity);
-    dataFile.print(",");
-    dataFile.print(LXLight);
-    dataFile.print(",");
-    dataFile.print(uvA);
-    dataFile.print(",");
-    dataFile.print(uvB);
-    dataFile.print(",");
-    dataFile.println(uvI);
-    // close the file
-    dataFile.close();
-
-    WiFiDrv::digitalWrite(27, LOW); //BLUE
+    if (cardStatus == true) {
+      // open the logfile
+      dataFile = SD.open("Testing.csv", FILE_WRITE);
+      WiFiDrv::digitalWrite(27, HIGH); //BLUE
+      // write to sd card each of the sensor values
+      dataFile.print(ntpftime);
+      dataFile.print(",");
+      dataFile.print(temperature);
+      dataFile.print(",");
+      dataFile.print(humidity);
+      dataFile.print(",");
+      dataFile.print(LXLight);
+      dataFile.print(",");
+      dataFile.print(uvA);
+      dataFile.print(",");
+      dataFile.print(uvB);
+      dataFile.print(",");
+      dataFile.println(uvI);
+      // close the file
+      dataFile.close();
+      WiFiDrv::digitalWrite(27, LOW); //BLUE
+    }
 
     
     
@@ -420,33 +496,71 @@ void loop() {
       display.display();
     }
     count = 0;
-    while (count != 10) {
-    ntpftime = timeClient.getFormattedTime();
-      WiFiDrv::digitalWrite(25, HIGH); //GREEN
-      webServer();
-      WiFiDrv::digitalWrite(25, LOW); //GREEN
-      delay(500);
-      count = count + 1;
-    
-      temperature = ENV.readTemperature();
-      humidity = ENV.readHumidity();
-      pressure = ENV.readPressure();
-      LXLight = ENV.readLux();
-      //UV
-      uvA = ENV.readUVA();
-      uvB = ENV.readUVB();
-      uvI = ENV.readUVIndex();
-    
-      display.clearDisplay();
-      display.setCursor(0, 0);
-      display.print("UVA: ");
-      display.println(uvA);
-      display.print("UVB: ");
-      display.println(uvB);
-      display.print("UVI: ");
-      display.println(uvI);
-      display.println(ntpftime);
-      display.display();
+
+    if (moreInfo == true) {
+      while (count != 10) {
+      ntpftime = timeClient.getFormattedTime();
+        WiFiDrv::digitalWrite(25, HIGH); //GREEN
+        webServer();
+        WiFiDrv::digitalWrite(25, LOW); //GREEN
+        delay(500);
+        count = count + 1;
+      
+        temperature = ENV.readTemperature();
+        humidity = ENV.readHumidity();
+        pressure = ENV.readPressure();
+        LXLight = ENV.readLux();
+        //UV
+        uvA = ENV.readUVA();
+        uvB = ENV.readUVB();
+        uvI = ENV.readUVIndex();
+      
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.print("UVA: ");
+        display.println(uvA);
+        display.print("UVB: ");
+        display.println(uvB);
+        display.print("UVI: ");
+        display.println(uvI);
+        display.println(ntpftime);
+        display.display();
+      }
+    }
+    else {
+      while (count != 10) {
+        WiFiDrv::digitalWrite(25, HIGH); //GREEN
+        webServer();
+        WiFiDrv::digitalWrite(25, LOW); //GREEN
+        delay(500);
+        count = count + 1;
+      
+        temperature = ENV.readTemperature();
+        humidity = ENV.readHumidity();
+        pressure = ENV.readPressure();
+        LXLight = ENV.readLux();
+        //UV
+        uvA = ENV.readUVA();
+        uvB = ENV.readUVB();
+        uvI = ENV.readUVIndex();
+      
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.print("Temperature: ");
+        display.print(temperature);
+        display.println("C");
+        display.print("Humidity: ");
+        display.print(humidity);
+        display.println("%");
+        display.print("Pressure: ");
+        display.print(pressure);
+        display.println("hPa");
+        display.print("Light: ");
+        display.print(LXLight);
+        display.print("LUX");
+        display.display();
+      }
+    count = 0;
     }
     count2 = count2 + 1;
   }
